5f654a244f7431e05cc52f973a9783fbd81c0f0e
Katie Wittine <kwittine@gmail.com>
2018-07-18 17:25:28 -0400

    Added a ViewBag

--- Capstone.Web/Controllers/SurveyController.cs
+++ Capstone.Web/Controllers/SurveyController.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Capstone.Web.DAL;
+using Capstone.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Capstone.Web.Controllers
 {
     public class SurveyController : Controller
     {
 		private ISurveyDAL surveyDAL;
+		private IParkDAL parkDAL;
 
-		public SurveyController(ISurveyDAL surveyDAL)
+		public SurveyController(ISurveyDAL surveyDAL, IParkDAL parkDAL)
 		{
 			this.surveyDAL = surveyDAL;
+			this.parkDAL = parkDAL;
 		}
 
         public IActionResult Index()
@@ -25,6 +29,11 @@ namespace Capstone.Web.Controllers
 
 		public IActionResult TakeSurvey()
 		{
+			var parks = parkDAL.GetAllParks();
+
+			var options = parks.Select<Park, SelectListItem>(park => new SelectListItem() { Text = park.ParkName, Value = park.ParkCode });
+			ViewBag.Parks = options;
+
 			return View();
 		}
     }
